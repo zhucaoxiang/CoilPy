19f8a5e5a07b3eb68a72b84079809b533709e87f
Caoxiang Zhu <czhu@pppl.gov>
2019-07-29 16:11:54 -0400

    revise __init_ to incorporate dipole

--- __init__.py
+++ __init__.py
@@ -11,6 +11,9 @@ import matplotlib.pyplot as plt
 from mayavi import mlab # to overrid plt.mlab
 import warnings
 import sys
+from pyevtk.hl import gridToVTK, pointsToVTK
+import pandas as pd
 
 # local packages
 from surface import *
+from dipole import *
