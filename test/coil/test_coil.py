ffaf8fbd50ae2fb5f2c40b65e120abe48fe5f38b
Caoxiang Zhu <zcxiang@gmail.com>
2021-02-20 11:22:16 +0800

    add a test case

---
+++ test/coil/test_coil.py
@@ -0,0 +1,28 @@
+from coilpy import Coil
+import numpy as np
+
+# read
+ellipse = Coil.read_makegrid("ellipse.coils")
+assert ellipse.num == 16, "Coil number is read incorrectly!"
+assert len(ellipse.data[0].x) == 129, "Segment number is read incorrectly!"
+assert ellipse.data[15].I == -1e6, "Coil current is read incorrectly!"
+assert ellipse.data[10].group == 3, "Coil group is read incorrectly!"
+
+# plot
+ellipse.plot(irange=range(0, 16, 4))
+ellipse.plot(irange=range(0, 16, 4), enginer="plotly", plot2d=True)
+
+# save VTK
+ellipse.toVTK("ellipse")
+ellipse.toVTK("ellipse", line=False, width=0.05, heigh=0.05)
+
+# calculate B field
+b = np.array([-5.85704462e-04, 2.94453517e-03, -1.63013362e-18])
+assert np.allclose(ellipse.data[0].bfield([0, 0, 0]), b)
+
+# misc
+ellipse.data[1].interpolate()
+ellipse.data[1].magnify(ratio=2.0)
+
+# save
+ellipse.save_makegrid("test.coils")
