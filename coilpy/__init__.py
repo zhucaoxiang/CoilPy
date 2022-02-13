dec608e769d24fa98bd1475c1cfa4cccdc5b4b21
Caoxiang Zhu <zcxiang@gmail.com>
2022-02-14 00:38:31 +0800

    add muse2magtense

--- coilpy/__init__.py
+++ coilpy/__init__.py
@@ -8,7 +8,7 @@ The repository is available at https://github.com/zhucaoxiang/CoilPy.
 
 For full documenation, please check https://zhucaoxiang.github.io/CoilPy/api/coilpy.html.
 """
-__version__ = "0.3.25"
+__version__ = "0.3.26"
 
 # local packages
 from .misc import colorbar, get_figure, kwargs2dict, map_matrix
@@ -29,6 +29,6 @@ from .mgrid import Mgrid
 from .pm4stell import blocks2vtk, blocks2ficus
 from .magnet import Magnet, corner2magnet
 from .magtense_interface import get_center, build_prism, blocks2tiles
-from .magtense_interface import corner2tiles, magtense2vtk
+from .magtense_interface import corner2tiles, magtense2vtk, muse2magntense
 
 # from coilpy_fortran import hanson_hirshman, biot_savart
