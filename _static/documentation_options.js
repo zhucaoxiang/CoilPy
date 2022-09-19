5129e4e2c6cb5b48e0196ebb556f0b2de8d05dcb
zhucaoxiang <zhucaoxiang@users.noreply.github.com>
2022-09-19 02:52:06 +0000

    deploy: 88549a9aa1ed148746cfb2b5011dd0fbd2db9073

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.33',
+    VERSION: '0.3.34',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
