ee01037cbf3b08e34406766d3cc1e0c6b2f42281
Caoxiang Zhu <zcxiang@gmail.com>
2022-11-15 14:11:56 +0800

    update version number; update import lines

--- coilpy/__init__.py
+++ coilpy/__init__.py
@@ -8,15 +8,10 @@ The repository is available at https://github.com/zhucaoxiang/CoilPy.
 
 For full documenation, please check https://zhucaoxiang.github.io/CoilPy/api/coilpy.html.
 """
-__version__ = "0.3.34"
+__version__ = "0.3.35"
 
 # local packages
-from .misc import colorbar, get_figure, kwargs2dict, map_matrix
-from .misc import print_progress, toroidal_period, vmecMN, xy2rp
-from .misc import trigfft, fft_deriv, trig2real, vmec2focus
-from .misc import real2trig_2d, booz2focus, read_focus_boundary, div0
-from .misc import biot_savart, rotation_matrix, rotation_angle
-from .misc import set_axes_equal, scan_focus, tracing, poincare_plot
+from .misc import *
 from .hdf5 import HDF5
 from .netcdf import Netcdf
 from .surface import FourSurf
@@ -29,8 +24,7 @@ from .booz_xform import BOOZ_XFORM
 from .mgrid import Mgrid
 from .pm4stell import blocks2vtk, blocks2ficus
 from .magnet import Magnet, corner2magnet
-from .magtense_interface import get_center, build_prism, blocks2tiles
-from .magtense_interface import corner2tiles, magtense2vtk, muse2magntense
+from .magtense_interface import *
 from .current_potential import Regcoil
 
 # from coilpy_fortran import hanson_hirshman, biot_savart
